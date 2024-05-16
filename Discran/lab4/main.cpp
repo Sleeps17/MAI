d44e2ac7384cab63fc0904cae9e0e061e8df10e5
Pasha <pavelloveski04@gmail.com>
2024-05-16 23:30:50 +0600

    OK

--- Discran/lab4/main.cpp
+++ Discran/lab4/main.cpp
@@ -1,9 +1,17 @@
-#include <sstream>
+#include "unordered_map"
+#include "sstream"
 #include "iostream"
 #include "vector"
 #include "utility"
 
-const int MAX = 256;
+struct Triple {
+    std::string word;
+    uint line_idx;
+    uint word_idx;
+};
+
+using Text = std::vector<Triple>;
+using Pattern = std::vector<std::string>;
 
 void to_lower_case(std::string& str) {
     for(size_t i = 0; i < str.length(); i++) {
@@ -11,99 +19,80 @@ void to_lower_case(std::string& str) {
     }
 }
 
-void badCharHeuristic(const std::string& str, int size, int bad_char[MAX]) {
-    for (int i = 0; i < MAX; i++) {
-        bad_char[i] = size;
-    }
-    for (int i = 0; i < size - 1; i++) {
-        bad_char[static_cast<unsigned char>(str[i])] = size - i - 1;
-    }
+int bad_symbol_heuristic_shift(std::unordered_map<std::string, int> &table, int i, const std::string &s) {
+    return table.count(s) == 0 ? i + 1 : (i - table[s] > 0 ? i - table[s] : 1);
 }
 
-std::vector<uint> search(const std::string& txt, const std::string& pat) {
-    std::vector<uint> answer;
+std::vector<std::pair<uint, uint>> search(const Text& text, const Pattern& pattern) {
+    std::vector<std::pair<uint, uint>> answer;
 
-    int n = static_cast<int>(txt.length());
-    int m = static_cast<int>(pat.length());
+    uint pattern_size = pattern.size();
+    uint text_size = text.size();
 
-    int bad_char[MAX];
-    badCharHeuristic(pat, m, bad_char);
+    std::unordered_map<std::string, int> stop_table;
+    for(size_t pattern_iterator = 0; pattern_iterator < pattern_size; pattern_iterator++) {
+        stop_table[pattern[pattern_iterator]] = static_cast<int>(pattern_iterator);
+    }
 
-    int s = 0;
-    while (s <= (n - m)) {
-        int j = m - 1;
-        while (j >= 0 && pat[j] == txt[s + j]) {
-            j--;
+    int i = static_cast<int>(pattern_size - 1);
+    while(i >= 0 && i < static_cast<int>(text_size)) {
+        int text_idx = i;
+        int pattern_idx = static_cast<int>(pattern_size - 1);
+
+        while(text[text_idx].word == pattern[pattern_idx]) {
+            if(pattern_idx == 0) {
+                answer.emplace_back(text[text_idx].line_idx, text[text_idx].word_idx);
+                break;
+            }
+
+            text_idx--;
+            pattern_idx--;
         }
-        if (j < 0) {
-            answer.push_back(s);
-            s += m;
-        } else {
-            s += bad_char[static_cast<unsigned char>(txt[s + j])] - m + j + 1 >= 1 ? bad_char[static_cast<unsigned char>(txt[s + j])] - m + j + 1 : 1;
-        }
+
+        i += std::max(1, bad_symbol_heuristic_shift(stop_table, pattern_idx, text[text_idx].word));
     }
 
     return answer;
 }
 
-std::vector<int> findPattern(const std::string& text, const std::string& pattern) {
-    std::vector<int> positions;
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
 
-    size_t pos = text.find(pattern, 0);
-    while (pos != std::string::npos) {
-        positions.push_back(pos);
-        pos = text.find(pattern, pos + 1);
+    Text text;
+    Pattern pattern;
+
+    std::string line;
+    std::getline(std::cin, line);
+    to_lower_case(line);
+
+    std::stringstream ss{line};
+    std::string word;
+    while(ss >> word) {
+        pattern.push_back(word);
     }
 
-    return positions;
-}
+    uint line_idx = 1;
+    while(std::getline(std::cin, line)) {
+        if (line.empty()) {
+            line_idx++;
+            continue;
+        }
 
-int main() {
-    std::string pattern;
-    std::getline(std::cin, pattern);
-    to_lower_case(pattern);
+        to_lower_case(line);
 
-    std::vector<uint> indexes;
-    std::vector<std::pair<int, int>> positions;
-
-    std::string text, line;
-    uint idx = 0, line_idx = 1, word_idx = 1;
-    while (std::getline(std::cin, line)){
-        std::stringstream iss(line);
-
-        std::string word;
-        while(iss >> word) {
-            indexes.push_back(idx);
-            positions.emplace_back(line_idx, word_idx);
-
-            idx += word.length() + 1;
+        ss = std::stringstream(line);
+        uint word_idx = 1;
+        while(ss >> word) {
+            text.push_back({word, line_idx, word_idx});
             word_idx++;
-
-            text += word + " ";
         }
 
         line_idx++;
-        word_idx = 1;
     }
 
-    text = text.substr(0, text.length() - 1);
-
-    to_lower_case(text);
-
-    auto answer = search(text, pattern);
-
-    for (auto& _idx : answer) {
-        if ((_idx != 0 && text[_idx-1] != ' ') || (_idx + pattern.length() != text.length() && text[_idx + pattern.length()] != ' ')) {
-            continue;
-        }
-        auto it = std::lower_bound(indexes.begin(), indexes.end(), _idx);
-        if (*it != _idx || it == indexes.end()) {
-            continue;
-        }
-        auto pos = std::distance(indexes.begin(), it);
-
-        std::cout << positions[pos].first << ", " << positions[pos].second << "\n";
+    for(auto [_line_idx, _word_idx] : search(text, pattern)) {
+        std::cout << _line_idx << ", " << _word_idx << "\n";
     }
-
-    return 0;
 }
